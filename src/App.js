b9d5071970d49420281d00036d5202c758d6cf26
Avni <avnipatil855@gmail.com>
2021-12-15 13:55:57 +0530

    updatd

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Home from './View/Home/Home'
 import './App.css';
 import Footer from './Layout/footer/footer';
 import 'bootstrap/dist/css/bootstrap.css';
+import Cards from './View/Components/Cards/Cards';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Cards/>
       <Footer />
     </div>
   );
