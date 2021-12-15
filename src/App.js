39416366bfebff80f97ed0414aa8d1f4e35cacec
Naeema21 <naeemabargir9@gmail.com>
2021-12-15 19:26:17 +0530

    header updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React, { Suspense } from 'react';
 import './App.css';
-import Footer from './Layout/footer/footer';
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Header from './Layout/Header/Header';
+import Footer from './Layout/footer/footer';
+import ScrollButton from './Layout/ScrollButton/ScrollButton'
+import ScrollToTop from './Layout/ScrollToTop/ScrollToTop'
 
 function App() {
   //home
   const Home = React.lazy(() => import('./View/Home/Home'))
-
   //Account
   const Account = React.lazy(()=>import('./View/Account/Account'))
   return (
     <div className="App">
       <BrowserRouter>
         <Suspense fallback={"loader"}>
+          <ScrollToTop />
+          <Header />
           <Switch>
             <Route exact path="/account" component={Account}></Route>
             <Route exact path="/" component={Home}></Route>
             <Route exact  component={Home}></Route>
           </Switch>
+          <ScrollButton />
           <Footer />
         </Suspense>
       </BrowserRouter>
--- src/Layout/Header/Header.js
+++ src/Layout/Header/Header.js
@@ -1,9 +1,71 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Navbar, Nav } from 'react-bootstrap'
+import '../layout.css'
 
-const Header =()=> {
+const Header = () => {
     return (
-        <div>
-            header
+        <div className='header'>
+            <div className='topbar py-2'>
+                <div className='container'>
+                    <div className='row align-items-center'>
+                        <div className='col-lg-6'>
+                            <p>Available 24/7 at <a href='tel:(00) 33 169 7720'> (00) 33 169 7720</a></p>
+                        </div>
+                        <div className='col-lg-6 d-flex justify-content-end'>
+                            <p>
+                                <span>
+                                    <Link to="/">
+                                        <i className='fa fa-heart-o text-danger me-2'></i>Wishlist(3)
+                                    </Link>
+                                </span> &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+                                <span>
+                                    <Link to="/">
+                                        <i className='fa fa-refresh text-danger me-2'></i>Compare(3)
+                                    </Link>
+                                </span> &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+                                <span>
+                                    <Link to="/">
+                                        <i className='fa fa-map-marker text-danger me-2'></i>Order Tracking
+                                    </Link>
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* top bar end */}
+            <Navbar collapseOnSelect expand="lg" >
+                <div className='container'>
+                    <Navbar.Brand href="#home">Daruwale</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#shop">Shop</Nav.Link>
+                            <Nav.Link href="#shop">Account</Nav.Link>
+
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#serch">
+                                <form className="search-bar">
+                                    <input type="search" placeholder="Search" />
+                                </form>
+                            </Nav.Link>
+                            <Nav.Link href="#deets">
+                                <span>
+                                    <i className='fa fa-user-o'> </i>&nbsp;Hello, sign In
+                                </span>
+                            </Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                <span>
+                                    <i className='fa fa-shopping-cart'> </i>&nbsp;My Cart
+                                </span>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </div>
+            </Navbar>
         </div>
     )
 }
