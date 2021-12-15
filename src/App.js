850b748dbb66c9031aa9790e1ea684c94423e1ff
Naeema21 <naeemabargir9@gmail.com>
2021-12-15 12:32:54 +0530

    product data added and route set

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import Home from './View/Home/Home'
+import React, { Suspense } from 'react';
 import './App.css';
 import Footer from './Layout/footer/footer';
 import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
+  //home
+  const Home = React.lazy(() => import('./View/Home/Home'))
   return (
     <div className="App">
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Suspense fallback={"loader"}>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+          </Switch>
+          <Footer />
+        </Suspense>
+      </BrowserRouter>
     </div>
   );
 }
