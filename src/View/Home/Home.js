aef06b356fbbee717e27b62bb18f55f133ee2a2e
Avni <avnipatil855@gmail.com>
2021-12-16 11:40:13 +0530

    cards hover issue done 3

--- src/View/Home/Home.js
+++ src/View/Home/Home.js
@@ -113,11 +113,7 @@ const Home = () => {
             </section>
             {/* Banner Completed */}
              {/* Product cards start */} 
-<<<<<<< HEAD
-             <section className="procard-sec">
-=======
              <section className="product-list">
->>>>>>> 52a62772e0540586ae9ed11b99532f37dd1f2704
                 <div className="container pt-5">
                     <div className="row">
                         <div className="col-lg-12 col-md-12 col-sm-12 d-flex flex-wrap justify-content-between align-items-center pt-1 border-bottom pb-4 mb-4">
@@ -125,13 +121,8 @@ const Home = () => {
                             <button className='cards-moreproduct-btn btn btn-sm'>More Products<i className="fa fa-angle-right" style={{fontSize:'15px', paddingLeft:'3px'}}></i></button>
                         </div> 
                         {/* Using Map Function to access the data & send to card */}
-<<<<<<< HEAD
                         {products.slice(0,8).map((productdata,i) => (
-                            <div className='col-lg-3 col-md-3 col-sm-3' key={i}>
-=======
-                        {products.slice(0,8).map((productdata) => (
-                            <div className='col-lg-3 col-md-4 col-sm-6'>
->>>>>>> 52a62772e0540586ae9ed11b99532f37dd1f2704
+                            <div className='col-lg-3 col-md-4 col-sm-6' key={i}>
                                 <Card category={productdata.category} name={productdata.name} price={productdata.price} imgsrc={productdata.imgsrc} star={productdata.star} />
                             </div>
                          ))}                       
